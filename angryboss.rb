91fd54c2bb803b8074eab81e3252035fe08274d7
Perry E. Henderson <hendersonperry96@yahoo.com>
2017-03-07 19:05:18 -0500

    ruby 2

---
+++ angryboss.rb
@@ -0,0 +1,4 @@
+names = ['johnson', 'smith', 'weinberg', 'filmore']
+puts "Can't you see I'm busy!! Make it fast dude, #{names[ rand (4)].upcase "
+request = gets.chomp
+puts "Whaddya Mean\" #{request.upcase}\"?!? You are so fired !!"
---
+++ favoritenumber.rb
@@ -0,0 +1,4 @@
+puts = "Hey ! What\'s your favorite number?"
+fav_num = gets.chomp.to_i
+better_num = fav_num + 1
+puts = "That's okay, I guess, but isn't # {fav_num + 1} just a bit better?"
